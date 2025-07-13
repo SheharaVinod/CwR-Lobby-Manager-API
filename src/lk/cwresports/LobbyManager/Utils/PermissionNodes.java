db30eaeb7287fe90f21976ad912df8e11447cd47
shehara winod <shehara.winod123@gmail.com>
2025-07-13 08:38:52 +0530

    Add Copyright claim

--- src/lk/cwresports/LobbyManager/Utils/PermissionNodes.java
+++ src/lk/cwresports/LobbyManager/Utils/PermissionNodes.java
@@ -1,3 +1,21 @@
+/*
+    CwR Lobby Manager API - Minecraft plugin for managing multiple spawn lobbies
+    Copyright (C) 2025 SheharaVinod(AKN Mr_Unknown), Team CwR
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package lk.cwresports.LobbyManager.Utils;
 
 public class PermissionNodes {
